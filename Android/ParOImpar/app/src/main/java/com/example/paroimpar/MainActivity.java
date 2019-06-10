24afd7770725f64c8915ce89d912fdc7767234c3
Carlos Dario Flores <cdfn3@hotmail.com>
2019-06-09 18:23:08 -0600

    Par o impar
    Android studio

--- Android/ParOImpar/app/src/main/java/com/example/paroimpar/MainActivity.java
+++ Android/ParOImpar/app/src/main/java/com/example/paroimpar/MainActivity.java
@@ -6,35 +6,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-EditText num;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //Creando Variables
+    public EditText num;
+    public TextView respuesta;
+    public Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Asignando R id
+        num = (EditText) findViewById(R.id.num);
+        respuesta=(TextView) findViewById(R.id.r);
+        btn=(Button) findViewById(R.id.btn);
+        //Asignando evento onclick al boton btn
+        btn.setOnClickListener(this);
+    }
 
-        num=(EditText) findViewById(R.id.num);
-        Button btn=(Button) findViewById(R.id.btn);
-        final TextView r = (TextView) findViewById(R.id.resultado);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int numero=Integer.valueOf(num.getText().toString());
-                if (numero % 2 == 0)
-                {
-                    r.setText("El numero introducido es par.");
-                }
-                else
-                {
-                    r.setText("El numero introducido es impar.");
-                }
-
-            }
-        });
+    @Override
+    public void onClick(View v) {
 
 
+        int numero=Integer.parseInt(num.getText().toString());
+        //Si el numero es impar lo mostraremos con un mensaje
+        if((numero % 2 ) ==0)
+        {
+            respuesta.setText("El numero es par");
+        }
+        else {
+            //Mostrando resultado por medio de un mensaje
+            Toast.makeText(this, "El numero es impar", Toast.LENGTH_LONG).show();
+        }
 
 
 
